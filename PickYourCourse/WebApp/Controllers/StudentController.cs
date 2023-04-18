b4b2a91d1da3c1a2b84c34c144851a639649f9b3
Valentyna Nochovna (ETIYA) <valentyna.nochovna@etiya.com>
2023-04-18 22:46:22 +0300

    student-home: add logout action

--- PickYourCourse/WebApp/Controllers/StudentController.cs
+++ PickYourCourse/WebApp/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 
 namespace WebApp.Controllers;
@@ -8,5 +10,12 @@ public class StudentController: Controller
     public IActionResult StudentHomePage(string email)
     {
         return View();
+    }
+    
+    public async Task<IActionResult> LogOut()
+    { 
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        
+        return RedirectToAction("Index", "Home");
     }
 }
