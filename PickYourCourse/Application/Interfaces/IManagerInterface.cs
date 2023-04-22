4b00e232639ca1e57a21437089036bdd5809cc85
Valentyna Nochovna (ETIYA) <valentyna.nochovna@etiya.com>
2023-04-22 22:12:15 +0300

    manager-professor-action: add aprove, reject professor functionality on manager home page, add data display

---
+++ PickYourCourse/Application/Interfaces/IManagerInterface.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces;
+
+public interface IManagerInterface
+{
+    Task ApproveProfessor(string email);
+    Task RejectProfessor(string email);
+}
--- PickYourCourse/WebApp/wwwroot/js/manager-home.js
+++ PickYourCourse/WebApp/wwwroot/js/manager-home.js
@@ -1,7 +1,13 @@
-﻿
-let el = document.getElementsByClassName("prof-btn");
+﻿let el = document.getElementsByClassName("prof-btn");
+const selectedProfessor = {
+    id: 0,
+    email: "",
+};
 for(let i = 0; i < el.length; i++) {
-    el[i].addEventListener("click", function () {
+    el[i].addEventListener("click", function (event) {
+        selectedProfessor.id = +event.target.id;
+        selectedProfessor.email = event.target.innerText;
+        console.log(selectedProfessor)
         document.getElementById("pagination").style.display = "none";
         document.getElementById("btns").style.display = "flex";
     });
@@ -11,3 +17,16 @@ document.getElementById("cancel-prof").addEventListener("click", function () {
     document.getElementById("pagination").style.display = "flex";
 });
 
+function professorAction(managersEmail, isApproval) {
+    $.ajax({
+        type: "POST",
+        url: `ProfessorAction`,
+        data: JSON.stringify({isApproval: isApproval, professorEmail: selectedProfessor.email, managerEmail: managersEmail}),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).done(function () {
+        window.location.href = "ManagerHomePage?email=" + managersEmail;
+    });
+}
