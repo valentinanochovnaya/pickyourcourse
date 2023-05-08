8f9442a38b9579ca4ce83b83a6d392b48bbfe44b
Valentyna Nochovna (ETIYA) <valentyna.nochovna@etiya.com>
2023-05-08 23:25:05 +0300

    add courses page, display courses depending on year, add styles

--- PickYourCourse/Application/Interfaces/IStudentInterface.cs
+++ PickYourCourse/Application/Interfaces/IStudentInterface.cs
@@ -1,6 +1,9 @@
-﻿namespace Application.Interfaces;
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
 
 public interface IStudentInterface
 {
-    
+    Task<Student> GetStudent(string email);
+    Task<List<Course>> GetStudentCourses(Student student);
 }
---
+++ PickYourCourse/Persistence/Services/StudentService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Services;
+
+public class StudentService: IStudentInterface
+{
+    private readonly DataContext.DataContext _context;
+    public StudentService(DataContext.DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Student> GetStudent(string email)
+    {
+        return await _context.Students?.FirstOrDefaultAsync(student => student.Email == email);
+    }
+
+    public async Task<List<Course>> GetStudentCourses(Student student)
+    {
+        return await _context.Courses.Where(course => course.Year == student.Year).ToListAsync();
+    }
+}
